4fe727d77bc310b8833b171fec58ac465e8eb272
Leonardo Medina <ext_lpmedina@falabella.cl>
2024-12-31 13:58:25 -0500

    12. InputSignal and HostBindings

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,15 +1,30 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, HostBinding, input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
   imports: [],
   templateUrl: './calculator-button.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrls: ['./calculator-button.component.css'],
   host: {
     'class': 'w-1/4 border-r border-b border-indigo-400',
     'attribute': 'hola-leo',
     //'data-sizes': '33vw',
   }
 })
-export class CalculatorButtonComponent {
+export class CalculatorButtonComponent implements OnInit {
+  public isCommand = input(false, {
+    transform: (value: boolean | string) => {
+      return typeof value === 'string' ? value === '' : value;
+    }
+  });
+
+  @HostBinding('class.is-command') get commandStyle() {
+    return this.isCommand();
+  }
+  ngOnInit(): void {
+    console.log(this.isCommand());
+  }
+
+
 }
